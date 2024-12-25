12e131eae2752eaee4af26dca4ba673abaa5c8b8
NimaShariatz <shariatz@ualberta.ca>
2024-12-25 16:08:11 -0700

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./components/Navigation"
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navigation/>
+
+      
+    </main>
+  )
+}
+export default App
