96d33bd360fc85a7fbec62d5cb1ccf673476ceec
NimaShariatz <shariatz@ualberta.ca>
2025-01-04 16:58:15 -0700

    did work on project card and grid layout

--- src/components/Project/Project_sect.jsx
+++ src/components/Project/Project_sect.jsx
@@ -10,15 +10,25 @@ const Project_sect = () => {
     useEffect(() => {
     
         gsap.to("#project_img_border", { height: '98%', duration: 3, ease:"back.out", delay: 1, opacity:1, onComplete: () => {
-            gsap.to("#project_img_border", { height: '91%', duration: 8, ease: "sine.inOut", yoyo: true, repeat: -1 });
-            gsap.to("#project_img_border", { height: '98%', duration: 8, ease: "sine.inOut", yoyo: true, repeat: -1, delay: 1 });
+            gsap.to("#project_img_border", { scaleY: '1.1', duration: 11, ease: "sine.inOut", yoyo: true, repeat: -1 });
+            gsap.to("#project_img_border", { scaleY: '1.18', duration: 11, ease: "sine.inOut", yoyo: true, repeat: -1});
         }});
     
     
-        gsap.to("#project_div", { height: '95%', duration: 3, ease:"back.out", delay: 1, opacity:1, onComplete: () => {
-            gsap.to("#project_div", { height: '85%', duration: 6, ease: "sine.inOut", yoyo: true, repeat: -1 });
-            gsap.to("#project_div", { height: '95%', duration: 6, ease: "sine.inOut", yoyo: true, repeat: -1, delay: 1 });
+        gsap.to("#project_div", { height: '100%', duration: 3, ease:"back.out", delay: 1, opacity:1, onComplete: () => {
+            gsap.to("#project_div", { scale: '1.1', duration: 8, ease: "sine.inOut", yoyo: true, repeat: -1 });
+            gsap.to("#project_div", { scale: '1.2', duration: 8, ease: "sine.inOut", yoyo: true, repeat: -1});
         }});
+
+
+        if (window.innerWidth > 760) {
+          gsap.to("#project_headerCard_container", { left: '80px', duration: 3, ease:"back.out", delay: 1 });
+
+          
+        }else{
+            gsap.to("#project_headerCard_container", { left: '9%', duration: 2, ease:"ease" });
+
+          }
     
     
       }, []);
@@ -26,7 +36,13 @@ const Project_sect = () => {
       
       useEffect(() => {
         const handleResize = () => {
-          console.log("z")
+          if (window.innerWidth > 760) {
+            gsap.to("#project_headerCard_container", { left: '80px', duration: 3, ease:"ease" });
+
+            
+          }else{
+              gsap.to("#project_headerCard_container", { left: '9%', duration: 2, ease:"ease" });
+          }
           
         };
     
@@ -67,7 +83,7 @@ const Project_sect = () => {
 
           <div className="linear_gradient_project" id="project_div" onMouseEnter={grow_project} onMouseLeave={normlize_project}></div>
     
-          <img className="project_img" src={project_pic} alt="University of Alberta"/>
+          <img className="project_img" src={project_pic} alt="RenewWool"/>
           <svg className="svg">{/*thanks to https://youtu.be/ybW7yUbOHN8?si=DC9LcMDlh42DWvin*/ }
               <clipPath id="clip_project" clipPathUnits="objectBoundingBox"><path d="M0.19,0.432 C0.109,0.541,0.117,0.406,0,0.451 V1 C0.074,0.956,0.223,1,0.316,0.92 C0.432,0.815,0.471,0.931,0.484,0.953 C0.511,0.983,0.608,0.948,0.679,0.973 C0.768,1,0.743,0.939,0.82,0.953 C0.882,0.964,0.966,0.866,1,0.815 V0.069 C0.967,0.033,0.874,-0.009,0.768,0.111 C0.636,0.26,0.665,-0.007,0.573,0.001 C0.48,0.01,0.495,0.486,0.432,0.385 C0.369,0.285,0.272,0.323,0.19,0.432"></path></clipPath>
           </svg>
@@ -83,7 +99,12 @@ const Project_sect = () => {
     
     
           <div className='grid_container'>
-            <div>1</div>
+            <div>
+              <img style={{borderRadius:"30px"}} src={project_pic} alt="University of Alberta"/>
+              <h2>ReneWool</h2>
+              <p>tgh</p>
+
+            </div>
             <div>2</div>
             <div>3</div>
             <div>4</div>
