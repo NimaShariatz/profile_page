94a8b3193681f6664e9cbbe75cf07f1200ca8f56
NimaShariatz <shariatz@ualberta.ca>
2025-01-05 22:49:45 -0700

    setup for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({server: {
-  watch: {
-    usePolling: true,
-  },
-},
-  plugins: [
-    react({
-      include: "**/*.jsx",
+export default defineConfig({
+  plugins: [react()],
+  base: "/profile_page"
+})
 
-    }),
-  ],
-});
+//used https://www.youtube.com/watch?v=hn1IkJk24ow for showing how to deploy on github pages properly
+
+
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   server: {
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+//   plugins: [
+//     react({
+//       include: "**/*.jsx",
+//     }),
+//   ],
+// });
